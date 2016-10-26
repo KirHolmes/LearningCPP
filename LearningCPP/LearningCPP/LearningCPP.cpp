1de1985823854568494217a012030856ef88d98d
KirHolmes <lihongbo8013@163.com>
2016-10-26 18:30:15 +0800

    [NEW] LearningCPP project in visual, LearningCPP.cpp

---
+++ LearningCPP/LearningCPP/LearningCPP.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+class point
+{
+public:
+	int x;
+	int y;
+
+	point();
+	point(int x, int y);
+	~point();
+
+	void output()
+	{
+		cout << x << endl << y << endl;
+	}
+private:
+
+};
+
+point::point()
+{
+	x = 0;
+	y = 0;
+
+}
+point::point(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+point::~point()
+{
+	delete &x;
+	delete &y;
+}
+int	main()
+{
+	point pt(1,2);
+	pt.output();
+	return 0;
+}
